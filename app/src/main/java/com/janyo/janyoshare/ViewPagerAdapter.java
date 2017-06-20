2973531eb925879a8b6de69d6a1ddcb001df8836
Mystery00 <mystery0dyl520@gmail.com>
2017-06-20 22:11:27 +0800

    代码重构完成

--- app/src/main/java/com/janyo/janyoshare/ViewPagerAdapter.java
+++ app/src/main/java/com/janyo/janyoshare/ViewPagerAdapter.java
@@ -8,32 +8,32 @@ import android.support.v4.app.Fragment;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
-	private List<Fragment> mFragments;
-	private List<String> mTitles;
+	private List<Fragment> fragmentList;
+	private List<String> titleList;
 
 	public ViewPagerAdapter(FragmentManager fm) {
 		super(fm);
-		mFragments = new ArrayList<>();
-		mTitles = new ArrayList<>();
+		fragmentList = new ArrayList<>();
+		titleList = new ArrayList<>();
 	}
 
 	public void addFragment(Fragment fragment, String title) {
-		mFragments.add(fragment);
-		mTitles.add(title);
+		fragmentList.add(fragment);
+		titleList.add(title);
 	}
 
 	@Override
 	public Fragment getItem(int position) {
-		return mFragments.get(position);
+		return fragmentList.get(position);
 	}
 
 	@Override
 	public int getCount() {
-		return mFragments.size();
+		return fragmentList.size();
 	}
 
 	@Override
 	public CharSequence getPageTitle(int position) {
-		return mTitles.get(position);
+		return titleList.get(position);
 	}
 }
